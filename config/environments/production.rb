a3eb04ce67f516263fb0b8b0f2e0f1be412ab896
El Flaco <redasterix89@gmail.com>
2014-09-05 18:57:21 -0500

    encode url / remove paperclip config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,13 +12,4 @@ config.cache_classes = true
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
   config.log_formatter = ::Logger::Formatter.new
-
-  config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_credentials => {
-      :bucket => ENV['S3_BUCKET_NAME'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
-  }
 end

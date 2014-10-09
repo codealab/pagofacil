0fc0f585dc2fa1b7d24b41f043a3e5559f3a8399
Javier Ibarrola <ibarroladt@gmail.com>
2014-10-09 11:59:47 -0500

    Change SSL to True

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,7 @@ config.cache_classes = true
   config.assets.version = '1.0'
   config.log_level = :info
   config.i18n.fallbacks = true
+  config.force_ssl = true
   config.active_support.deprecation = :notify
   config.log_formatter = ::Logger::Formatter.new
 end
